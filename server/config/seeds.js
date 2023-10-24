8e4a0069daa4c748d1277b286ec09ca66305da8a
Thiago Dantas <thiagoo.dantas11@gmail.com>
2023-10-24 19:17:51 +1100

    Adding code to seeds.js

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -24,133 +24,135 @@ db.once("open", async () => {
 
   const vinyls = await Vinyl.insertMany([
     {
-      title: "Tin of Cookies",
-      artist:
-        "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+      title: "YELLOW SUBMARINE (LP)",
+      artist: "The Beatles",
       user: users[0]._id,
-      description: "test",
-      image: "cookie-tin.jpg",
-      price: 2.99,
-      trackList: "test",
+      description:
+        "Yellow Submarine was the tenth studio album by the Beatles, originally released on 13 January 1969 in the United States and on 17 January 1969 in the United Kingdom.",
+      // image: "cookie-tin.jpg",
+      price: 53.99,
+      trackList:
+        "Yellow Submarine, Only A Northern Song, All Together Now, Hey Bulldog, It's All Too Much, All You Need Is Love, Pepperland, Sea Of Time, Sea Of Holes, Sea Of Monsters, March Of The Meanies, Pepperland Laid Waste, Yellow Submarine In Pepperland",
       genre: genres[0]._id,
-      listingDate: "10/11/1999",
+      listingDate: "10/10/2023",
     },
     {
-      title: "Tin of Cookies",
-      artist:
-        "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
-      user: users[0]._id,
-      description: "test",
-      image: "cookie-tin.jpg",
-      price: 2.99,
-      trackList: "test",
-      genre: genres[0]._id,
-      listingDate: "10/11/1999",
+      title: "MASTER OF PUPPETS (BATTERY BRICK LP)",
+      artist: "Metallica",
+      user: users[1]._id,
+      description:
+        "UMR/Mercury are pressing 11 of Metallica's studio album back catalogue on custom coloured vinyl for the first time for World ex North America.",
+      // image: "cookie-tin.jpg",
+      price: 88.99,
+      trackList:
+        "1. Battery, Master Of Puppets, The Thing That Should Not Be, Welcome Home (Sanitarium), Disposable Heroes, Leper Messiah, Orion, Damage Inc.",
+      genre: genres[1]._id,
+      listingDate: "01/10/2023",
     },
-    {
-      title: "Tin of Cookies",
-      artist:
-        "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
-      user: users[0]._id,
-      description: "test",
-      image: "cookie-tin.jpg",
-      price: 2.99,
-      trackList: "test",
-      genre: genres[0]._id,
-      listingDate: "10/11/1999",
-    },
-    {
-      title: "Tin of Cookies",
-      artist:
-        "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
-      user: users[0]._id,
-      description: "test",
-      image: "cookie-tin.jpg",
-      price: 2.99,
-      trackList: "test",
-      genre: genres[0]._id,
-      listingDate: "10/11/1999",
-    },
-    {
-      title: "Tin of Cookies",
-      artist:
-        "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
-      user: users[0]._id,
-      description: "test",
-      image: "cookie-tin.jpg",
-      price: 2.99,
-      trackList: "test",
-      genre: genres[0]._id,
-      listingDate: "10/11/1999",
-    },
-    {
-      title: "Tin of Cookies",
-      artist:
-        "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
-      user: users[0]._id,
-      description: "test",
-      image: "cookie-tin.jpg",
-      price: 2.99,
-      trackList: "test",
-      genre: genres[0]._id,
-      listingDate: "10/11/1999",
-    },
-    {
-      title: "Tin of Cookies",
-      artist:
-        "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
-      user: users[0]._id,
-      description: "test",
-      image: "cookie-tin.jpg",
-      price: 2.99,
-      trackList: "test",
-      genre: genres[0]._id,
-      listingDate: "10/11/1999",
-    },
-    {
-      title: "Tin of Cookies",
-      artist:
-        "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
-      user: users[0]._id,
-      description: "test",
-      image: "cookie-tin.jpg",
-      price: 2.99,
-      trackList: "test",
-      genre: genres[0]._id,
-      listingDate: "10/11/1999",
-    },
-    {
-      title: "Tin of Cookies",
-      artist:
-        "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
-      user: users[0]._id,
-      description: "test",
-      image: "cookie-tin.jpg",
-      price: 2.99,
-      trackList: "test",
-      genre: genres[0]._id,
-      listingDate: "10/11/1999",
-    },
-    {
-      title: "Tin of Cookies",
-      artist:
-        "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
-      user: users[0]._id,
-      description: "test",
-      image: "cookie-tin.jpg",
-      price: 2.99,
-      trackList: "test",
-      genre: genres[0]._id,
-      listingDate: "10/11/1999",
-    },
+    // {
+    //   title: "Tin of Cookies",
+    //   artist:
+    //     "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+    //   user: users[0]._id,
+    //   description: "test",
+    //   image: "cookie-tin.jpg",
+    //   price: 2.99,
+    //   trackList: "test",
+    //   genre: genres[0]._id,
+    //   listingDate: "10/11/1999",
+    // },
+    // {
+    //   title: "Tin of Cookies",
+    //   artist:
+    //     "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+    //   user: users[0]._id,
+    //   description: "test",
+    //   image: "cookie-tin.jpg",
+    //   price: 2.99,
+    //   trackList: "test",
+    //   genre: genres[0]._id,
+    //   listingDate: "10/11/1999",
+    // },
+    // {
+    //   title: "Tin of Cookies",
+    //   artist:
+    //     "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+    //   user: users[0]._id,
+    //   description: "test",
+    //   image: "cookie-tin.jpg",
+    //   price: 2.99,
+    //   trackList: "test",
+    //   genre: genres[0]._id,
+    //   listingDate: "10/11/1999",
+    // },
+    // {
+    //   title: "Tin of Cookies",
+    //   artist:
+    //     "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+    //   user: users[0]._id,
+    //   description: "test",
+    //   image: "cookie-tin.jpg",
+    //   price: 2.99,
+    //   trackList: "test",
+    //   genre: genres[0]._id,
+    //   listingDate: "10/11/1999",
+    // },
+    // {
+    //   title: "Tin of Cookies",
+    //   artist:
+    //     "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+    //   user: users[0]._id,
+    //   description: "test",
+    //   image: "cookie-tin.jpg",
+    //   price: 2.99,
+    //   trackList: "test",
+    //   genre: genres[0]._id,
+    //   listingDate: "10/11/1999",
+    // },
+    // {
+    //   title: "Tin of Cookies",
+    //   artist:
+    //     "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+    //   user: users[0]._id,
+    //   description: "test",
+    //   image: "cookie-tin.jpg",
+    //   price: 2.99,
+    //   trackList: "test",
+    //   genre: genres[0]._id,
+    //   listingDate: "10/11/1999",
+    // },
+    // {
+    //   title: "Tin of Cookies",
+    //   artist:
+    //     "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+    //   user: users[0]._id,
+    //   description: "test",
+    //   image: "cookie-tin.jpg",
+    //   price: 2.99,
+    //   trackList: "test",
+    //   genre: genres[0]._id,
+    //   listingDate: "10/11/1999",
+    // },
+    // {
+    //   title: "Tin of Cookies",
+    //   artist:
+    //     "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.",
+    //   user: users[0]._id,
+    //   description: "test",
+    //   image: "cookie-tin.jpg",
+    //   price: 2.99,
+    //   trackList: "test",
+    //   genre: genres[0]._id,
+    //   listingDate: "10/11/1999",
+    // },
   ]);
 
   console.log("products seeded");
 
   await User.create({
-    firstName: "Pamela",
-    lastName: "Washington",
-    email: "pamela@testmail.com",
+    firstName: "John",
+    lastName: "Doe",
+    email: "john@testmail.com",
     password: "password12345",
     listings: [
       {
@@ -160,9 +162,9 @@ db.once("open", async () => {
   });
 
   await User.create({
-    firstName: "Elijah",
-    lastName: "Holt",
-    email: "eholt@testmail.com",
+    firstName: "Jane",
+    lastName: "Doe",
+    email: "jane@testmail.com",
     password: "password12345",
     listings: [
       {
