7235ca945b2ad4fe0e835c0005a62fcb2f084c89
Andrei <andrei.ribeirow@gmail.com>
2023-10-28 11:51:45 +1100

    styling signup page an login page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import Auth from '../utils/auth';
-import { ADD_USER } from '../utils/mutations';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import Auth from "../utils/auth";
+import { ADD_USER } from "../utils/mutations";
 
 function Signup(props) {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ email: "", password: "" });
   const [addUser] = useMutation(ADD_USER);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    const {data} = await addUser({
+    const { data } = await addUser({
       variables: {
         email: formState.email,
         password: formState.password,
@@ -31,56 +31,70 @@ function Signup(props) {
   };
 
   return (
-    <div className="container my-1">
-      <Link to="/login">← Go to Login</Link>
-
-      <h2>Signup</h2>
-      <form onSubmit={handleFormSubmit}>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="firstName">First Name:</label>
-          <input
-            placeholder="First"
-            name="firstName"
-            type="firstName"
-            id="firstName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="lastName">Last Name:</label>
-          <input
-            placeholder="Last"
-            name="lastName"
-            type="lastName"
-            id="lastName"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="email">Email:</label>
-          <input
-            placeholder="youremail@test.com"
-            name="email"
-            type="email"
-            id="email"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row space-between my-2">
-          <label htmlFor="pwd">Password:</label>
-          <input
-            placeholder="******"
-            name="password"
-            type="password"
-            id="pwd"
-            onChange={handleChange}
-          />
-        </div>
-        <div className="flex-row flex-end">
-          <button type="submit">Submit</button>
-        </div>
-      </form>
+    <div className="signupPage">
+      <div className="signupPage__background"></div>
+      <img className="signupPage__logo" src="./images/vinyl-logo.jpg" alt="logo"></img>
+      <button className="signupPage__button">Sign In</button>
+      <div className="signupPage__gradient"></div>
+      <div className="signupPage__body">
+        <>
+        <h1> Unlimited vinyls, artists, genres and more.</h1>
+        <h2>Buy and sell all of your favorite vinyls</h2>
+        <h3>Ready to start? Enter your details to sign up and get full access</h3>
+        </>
+      </div>
     </div>
+
+    // <div className="container my-1">
+    //   <Link to="/login">← Go to Login</Link>
+
+    //   <h2>Signup</h2>
+    //   <form onSubmit={handleFormSubmit}>
+    //     <div className="flex-row space-between my-2">
+    //       <label htmlFor="firstName">First Name:</label>
+    //       <input
+    //         placeholder="First"
+    //         name="firstName"
+    //         type="firstName"
+    //         id="firstName"
+    //         onChange={handleChange}
+    //       />
+    //     </div>
+    //     <div className="flex-row space-between my-2">
+    //       <label htmlFor="lastName">Last Name:</label>
+    //       <input
+    //         placeholder="Last"
+    //         name="lastName"
+    //         type="lastName"
+    //         id="lastName"
+    //         onChange={handleChange}
+    //       />
+    //     </div>
+    //     <div className="flex-row space-between my-2">
+    //       <label htmlFor="email">Email:</label>
+    //       <input
+    //         placeholder="youremail@test.com"
+    //         name="email"
+    //         type="email"
+    //         id="email"
+    //         onChange={handleChange}
+    //       />
+    //     </div>
+    //     <div className="flex-row space-between my-2">
+    //       <label htmlFor="pwd">Password:</label>
+    //       <input
+    //         placeholder="******"
+    //         name="password"
+    //         type="password"
+    //         id="pwd"
+    //         onChange={handleChange}
+    //       />
+    //     </div>
+    //     <div className="flex-row flex-end">
+    //       <button type="submit">Submit</button>
+    //     </div>
+    //   </form>
+    // </div>
   );
 }
 
