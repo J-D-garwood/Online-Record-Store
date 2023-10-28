6a6dafb8ca2bce65035d87e6a54a2f8deb63c104
Thiago Dantas <thiagoo.dantas11@gmail.com>
2023-10-29 08:22:35 +1100

    addind code to 404 page

--- client/src/pages/404.jsx
+++ client/src/pages/404.jsx
@@ -1,18 +1,31 @@
 import Jumbotron from "../components/Jumbotron";
+import backgroundImage from "../images/404-error-page.jpg";
 
 const NoMatch = () => {
+  const containerStyle = {
+    backgroundImage: `url(${backgroundImage})`, // Set the background image
+    // backgroundSize: 'cover', // Adjust the size as needed
+    backgroundRepeat: 'no-repeat', // Adjust the repetition as needed
+    backgroundPosition: 'center', // Adjust the position as needed
+    backgroundColor: 'black',
+    height: '100vh',
+    
+    };
   return (
-    <div>
+    <div style={containerStyle}>
       <Jumbotron>
-        <h1>404 Page Not Found</h1>
-        <h1>
-          <span role="img" aria-label="Face With Rolling Eyes Emoji">
-            🙄
-          </span>
-        </h1>
+
       </Jumbotron>
     </div>
   );
 };
 
 export default NoMatch;
+
+
+        {/* <h1>404 Page Not Found</h1>
+        <h1>
+          <span role="img" aria-label="Face With Rolling Eyes Emoji">
+            🙄
+          </span>
+        </h1> */}
