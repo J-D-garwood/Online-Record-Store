b737487ecfb29fe7de1a5fb2011632559193c927
Andrei <andrei.ribeirow@gmail.com>
2023-10-29 12:13:36 +1100

    Queries fix

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -30,7 +30,7 @@ export const QUERY_ALL_VINYLS =gql`
     }
 `;
 
-/*
+
 export const QUERY_CHECKOUT = gql`
     query getCheckout($vinyls: [VinylInput]) {
         checkout(vinyls: $vinyls) {
@@ -39,24 +39,24 @@ export const QUERY_CHECKOUT = gql`
     }
 `;
 
-export const QUERY_ALL_VINYLS =gql`
-    {
-        vinyls {
-            _id
-            title
-            artist
-            description
-            image
-            price
-            trackList
-            genre {
-              _id
-            }
-            sold
-            listingDate
-        }
-    }
-`;
+// export const QUERY_ALL_VINYLS =gql`
+//     {
+//         vinyls {
+//             _id
+//             title
+//             artist
+//             description
+//             image
+//             price
+//             trackList
+//             genre {
+//               _id
+//             }
+//             sold
+//             listingDate
+//         }
+//     }
+// `;
 
 export const QUERY_GENRES = gql`
     {
@@ -91,4 +91,4 @@ export const QUERY_USER =gql`
             }
         } 
     }
-`*/
+`
